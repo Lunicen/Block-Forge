d68ab10c2e0479c1682a7b3f1414069689757913
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-05-15 21:48:48 +0200

    Add Log functionality

--- src/Core/Log.cpp
+++ src/Core/Log.cpp
@@ -1,1 +1,32 @@
 #include "Log.h"
+
+void Log::Init()
+{
+	spdlog::set_pattern("[%T] [%^%l%$] %v");
+	spdlog::info("Logging initialized!");
+}
+
+void Log::Trace(const std::string& message)
+{
+	spdlog::trace(message);
+}
+
+void Log::Info(const std::string& message)
+{
+	spdlog::info(message);
+}
+
+void Log::Warn(const std::string& message)
+{
+	spdlog::warn(message);
+}
+
+void Log::Error(const std::string& message)
+{
+	spdlog::error(message);
+}
+
+void Log::Critical(const std::string& message)
+{
+	spdlog::critical(message);
+}
--- src/Core/Log.h
+++ src/Core/Log.h
@@ -3,6 +3,12 @@
 
 class Log
 {
+public:
+	static void Init();
 
+	static void Trace(const std::string& message);
+	static void Info(const std::string& message);
+	static void Warn(const std::string& message);
+	static void Error(const std::string& message);
+	static void Critical(const std::string& message);
 };
-
