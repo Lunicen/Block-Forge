0e3e17f7f736c5160944193580c803695e3bed83
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-07-16 17:51:52 +0200

    Standarize paths and setup clean config

--- setup.bat
+++ setup.bat
@@ -7,53 +7,57 @@ set GLAD_build_dir=.\lib\glad\build
 
 :: Remove previous setup if exists
 echo Removing previous build if exists
-if exist %GLFW_build_dir% del %GLFW_build_dir%_Win32\*.* /s /q > nul 2>&1
-if exist %GLFW_build_dir% rmdir %GLFW_build_dir%_Win32 /s /q > nul 2>&1
-if exist %GLFW_build_dir% del %GLFW_build_dir%_x64\*.* /s /q > nul 2>&1
-if exist %GLFW_build_dir% rmdir %GLFW_build_dir%_x64 /s /q > nul 2>&1
+if exist %GLFW_build_dir% del %GLFW_build_dir%\Win32\*.* /s /q > nul 2>&1
+if exist %GLFW_build_dir% rmdir %GLFW_build_dir%\Win32 /s /q > nul 2>&1
+if exist %GLFW_build_dir% del %GLFW_build_dir%\x64\*.* /s /q > nul 2>&1
+if exist %GLFW_build_dir% rmdir %GLFW_build_dir%\x64 /s /q > nul 2>&1
 
-if exist %GLAD_build_dir% del %GLAD_build_dir%_Win32\*.* /s /q > nul 2>&1
-if exist %GLAD_build_dir% rmdir %GLAD_build_dir%_Win32 /s /q > nul 2>&1
-if exist %GLAD_build_dir% del %GLAD_build_dir%_x64\*.* /s /q > nul 2>&1
-if exist %GLAD_build_dir% rmdir %GLAD_build_dir%_x64 /s /q > nul 2>&1
+if exist %GLAD_build_dir% del %GLAD_build_dir%\Win32\*.* /s /q > nul 2>&1
+if exist %GLAD_build_dir% rmdir %GLAD_build_dir%\Win32 /s /q > nul 2>&1
+if exist %GLAD_build_dir% del %GLAD_build_dir%\x64\*.* /s /q > nul 2>&1
+if exist %GLAD_build_dir% rmdir %GLAD_build_dir%\x64 /s /q > nul 2>&1
 
 :: Check if the library is downloaded
-for /F %%i in ('dir /b /a "lib\glfw*"') do (
+for /F %%i in ('dir /b /a "%GLFW_root_dir%\*"') do (
     echo GLFW is downloaded!
-    goto :allModulesInstalled
+	goto :checkGLAD
 )
+echo GLFW module is missing!
 
-for /F %%i in ('dir /b /a "lib\glad*"') do (
+:checkGLAD
+for /F %%i in ('dir /b /a "%GLAD_root_dir%\*"') do (
     echo GLAD is downloaded!
-    goto :allModulesInstalled
+    goto :allSubmodulesInstalled
 )
+echo GLAD module is missing!
 
+:installMissingSubmodules
 :: Install missing dependencies
 echo Some modules are missing! Downloading...
 git submodule update --init --recursive
 git pull --recurse-submodules
 
-:allModulesInstalled
+:allSubmodulesInstalled
 echo Installing GLFW!
 
 :: x64 build
-cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A x64 -B %GLFW_build_dir%_x64
-start "GLFW Debug build x64" cmd /c "cmake --build %GLFW_build_dir%_x64 --config Debug"
-start "GLFW Release build x64" cmd /c "cmake --build %GLFW_build_dir%_x64 --config Release"
+cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A x64 -B %GLFW_build_dir%\x64
+start "GLFW Debug build x64" cmd /c "cmake --build %GLFW_build_dir%\x64 --config Debug --clean-first"
+start "GLFW Release build x64" cmd /c "cmake --build %GLFW_build_dir%\x64 --config Release --clean-first"
 
 :: Win32 build
-cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A Win32 -B %GLFW_build_dir%_Win32
-start "GLFW Debug build Win32" cmd /c "cmake --build %GLFW_build_dir%_Win32 --config Debug"
-start "GLFW Release build Win32" cmd /c "cmake --build %GLFW_build_dir%_Win32 --config Release"
+cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A Win32 -B %GLFW_build_dir%\Win32
+start "GLFW Debug build Win32" cmd /c "cmake --build %GLFW_build_dir%\Win32 --config Debug --clean-first"
+start "GLFW Release build Win32" cmd /c "cmake --build %GLFW_build_dir%\Win32 --config Release --clean-first"
 
 echo Installing GLAD!
 
 :: x64 build
-cmake %GLAD_root_dir% -B %GLAD_build_dir%_x64 -A x64
-start "GLAD Debug build x64" cmd /c "cmake --build %GLAD_build_dir%_x64 --config Debug --clean-first"
-start "GLAD Release build x64" cmd /c "cmake --build %GLAD_build_dir%_x64 --config Release --clean-first"
+cmake %GLAD_root_dir% -B %GLAD_build_dir%\x64 -A x64
+start "GLAD Debug build x64" cmd /c "cmake --build %GLAD_build_dir%\x64 --config Debug --clean-first"
+start "GLAD Release build x64" cmd /c "cmake --build %GLAD_build_dir%\x64 --config Release --clean-first"
 
 :: Win32 build
-cmake %GLAD_root_dir% -B %GLAD_build_dir%_Win32 -A Win32
-start "GLAD Debug build Win32" cmd /c "cmake --build %GLAD_build_dir%_Win32 --config Debug --clean-first"
-start "GLAD Release build Win32" cmd /c "cmake --build %GLAD_build_dir%_Win32 --config Release --clean-first"
+cmake %GLAD_root_dir% -B %GLAD_build_dir%\Win32 -A Win32
+start "GLAD Debug build Win32" cmd /c "cmake --build %GLAD_build_dir%\Win32 --config Debug --clean-first"
+start "GLAD Release build Win32" cmd /c "cmake --build %GLAD_build_dir%\Win32 --config Release --clean-first"
