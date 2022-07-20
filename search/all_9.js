076181ac24423a07a7359e1281a60e27861b2e83
RooTender <RooTender@users.noreply.github.com>
2022-07-20 15:43:34 +0000

    deploy: c29d1ea40f4e98e5c6a2faab6199147af1e04325

--- search/all_9.js
+++ search/all_9.js
@@ -1,5 +1,11 @@
 var searchData=
 [
-  ['save_0',['Save',['../class_metadata.html#a769e7b5c919ebd15354303e10f6fe92c',1,'Metadata']]],
-  ['setfilename_1',['SetFilename',['../class_metadata.html#ad84e37040e02575b907895212baa9bf6',1,'Metadata']]]
+  ['save_0',['Save',['../class_metadata.html#aaa0dcf0d919e873395538fae0b26b1b6',1,'Metadata']]],
+  ['setbool_1',['SetBool',['../class_metadata.html#a63bf33687ae931bb706cfdccd61e48d3',1,'Metadata']]],
+  ['setdouble_2',['SetDouble',['../class_metadata.html#a2069322f9e0695232740ad64bd8ff56a',1,'Metadata']]],
+  ['setfilename_3',['SetFilename',['../class_metadata.html#ad84e37040e02575b907895212baa9bf6',1,'Metadata']]],
+  ['setint_4',['SetInt',['../class_metadata.html#a307ee88b93f1f2a3909a1c736341f4b2',1,'Metadata']]],
+  ['setjsonobject_5',['SetJsonObject',['../class_metadata.html#a8c5559751c2701467289c997bed4e791',1,'Metadata']]],
+  ['setnull_6',['SetNull',['../class_metadata.html#aefd89080abb7532d82b748575094b25f',1,'Metadata']]],
+  ['setstring_7',['SetString',['../class_metadata.html#a988d7a62d4642bf6e7cdbfd56e832703',1,'Metadata']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['info_0',['Info',['../class_log.html#abc92ed5074f74feecedfac4f6dfa592c',1,'Log']]],
-  ['init_1',['Init',['../class_log.html#adc2470e1e4de07f81462116f6fdf60fd',1,'Log']]]
+  ['isloaded_1',['IsLoaded',['../class_metadata.html#a9efe9ca5aac6b06192ff54370ecbd348',1,'Metadata']]],
+  ['isnull_2',['IsNull',['../class_metadata.html#aff85bfd7270d0fac9fc3f2a036d43b54',1,'Metadata']]],
+  ['issaved_3',['IsSaved',['../class_metadata.html#af5d619efd7c6df8f3feb6761a6d037e7',1,'Metadata']]]
 ];
