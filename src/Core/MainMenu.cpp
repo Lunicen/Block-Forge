143a964ec26634e072840de5b0c03813230e2470
MadTechnocrat <kuba.jawor@tlen.pl>
2022-07-24 21:34:14 +0200

    test

--- src/Core/MainMenu.cpp
+++ src/Core/MainMenu.cpp
@@ -1,11 +1,4 @@
 #include "MainMenu.h"
-#include"imgui.h"
-#include"backends/imgui_impl_glfw.h"
-#include"backends/imgui_impl_opengl3.h"
-
-#include<iostream>
-#include<glad/glad.h>
-#include<GLFW/glfw3.h>
 
 
 void MainMenu::DrawCenteredText(const std::string text)
@@ -30,79 +23,106 @@ bool MainMenu::DrawWindowSizedButton(const std::string text)
 
 void MainMenu::InitializeGlfw()
 {
+	//version 3.3
+	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //(core profile (use only modern functions to hell with old ones))
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+void MainMenu::InitializeImgui(GLFWwindow* &window)
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+}
+
+void MainMenu::SetImguiBackgroundStyle(BackgroundStyle backgroundStyle)
+{
+	switch (backgroundStyle)
+	{
+	case BackgroundStyle::dark:
+		ImGui::StyleColorsDark();
+		break;
+
+	case BackgroundStyle::light:
+		ImGui::StyleColorsLight();
+		break;
+
+	case BackgroundStyle::classic:
+		ImGui::StyleColorsClassic();
+		break;
+	}
+}
+
+bool MainMenu::HasWindowFailedToCreate(GLFWwindow * &window)
+{
+	return window == nullptr;
 }
 
 
 void MainMenu::Draw()
 {
-	//initialize GLFW
-	glfwInit();
-
-	//hints (information about version to glfw) //version 3.3
 	InitializeGlfw();
 
-	GLFWwindow* window = glfwCreateWindow(1600, 800, "Main menu", /*glfwGetPrimaryMonitor()*/  nullptr, nullptr); //width, height, name, FULLSCREAN(nullptr/?), "not important"
-	//check if window fails to create
-	if (window == nullptr)
+	//In the future window should open in fullscreen OR/AND in window
+	//monitor: /*glfwGetPrimaryMonitor()*/
+	GLFWwindow* window = glfwCreateWindow(1600, 800, "Main menu", nullptr, nullptr); 
+	
+	if (HasWindowFailedToCreate(window))
 	{
-		std::cout << " Failed to generate GLFW window" << std::endl;
+		log.Critical("Main Menu failed to generate GLFW window!");
 		glfwTerminate();
+
 	}
 
-	glfwMakeContextCurrent(window); //introduce new window to current context
+	glfwMakeContextCurrent(window);
 
-	gladLoadGL(); //load GLAD so it configures OPENGL
-	//glViewport(0, 0, 800, 800); //area of the window for opengl speicfy the viewport from 0 to 800
+	InitializeImgui(window);
 
-
-	///////////////IMGUI/////////////////
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	//Alternatively:
-	//ImGui::StyleColorsClassic();
-	//ImGui::StyleColorsLight();
-
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
-	//////////////IMGUI///////////////////
+	gladLoadGL();
 
 
-	///MAIN WHILE LOOP///
 	bool quit = false;
 	while (!glfwWindowShouldClose(window) && !quit)
 	{
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f); //rgb, alpha
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//IMGUI
+
+
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
-		//ImGui::SetNextWindowSize(ImVec2(600, 500));
-
-
-		//For Options:
-		//ImGui::Text("Welcome to the Block Forge!");
-		//ImGui::Checkbox("Draw Triangle", &drawTriangle);
-		//ImGui::SliderFloat("Size", &size, 0.5f, 2.0f);
-		//ImGui::ColorEdit4("Color", color);
-		//ImGui::SetCursorPos(ImVec2(20, 20));
-
 
 		ImGui::Begin("Main Menu");
+
+
+		SetImguiBackgroundStyle(BackgroundStyle::dark);
+
 		DrawCenteredText("Welcome to the Block Forge!");
-		DrawWindowSizedButton("PLAY");
-		DrawWindowSizedButton("OPTIONS");
-		quit = DrawWindowSizedButton("EXIT");
+
+		if(DrawWindowSizedButton("PLAY"))
+		{
+			//TODO 
+		}
+
+		if(DrawWindowSizedButton("OPTIONS"))
+		{
+			//TODO
+		}
+
+		if(DrawWindowSizedButton("EXIT"))
+		{
+			//TODO ASK user whether to close
+			quit = true;
+		}
 
 		if(DrawWindowSizedButton("CREDITS"))
 		{
-			
+			//TODO
 		}
 
 		ImGui::End();
@@ -111,10 +131,9 @@ void MainMenu::Draw()
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 
-		//swap the front buffer with the back buffer
-		glfwSwapBuffers(window); 
-		//take care of all GLFW EVENTS
-		glfwPollEvents(); //process windows events: resize and so on... without this nothing will work
+
+		glfwSwapBuffers(window);  	//this swaps buffers without them them new frame will not be loaded
+		glfwPollEvents();			//process windows events: resize and so on... without this nothing will work
 	}
 
 	ImGui_ImplOpenGL3_Shutdown();
--- src/Core/MainMenu.h
+++ src/Core/MainMenu.h
@@ -1,12 +1,33 @@
 #pragma once
 #include <string>
 
+#include"imgui.h"
+#include"backends/imgui_impl_glfw.h"
+#include"backends/imgui_impl_opengl3.h"
+// ReSharper disable once CppUnusedIncludeDirective
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "Log.h"
+
+enum class BackgroundStyle
+{
+	dark = 0,
+	light = 1,
+	classic = 2
+};
+
 class MainMenu
 {
 private:
+	Log& log = Log::Get();
+
 	void DrawCenteredText(const std::string text);
 	bool DrawWindowSizedButton(const std::string text);
 	void InitializeGlfw();
+	void InitializeImgui(GLFWwindow* &window);
+	void SetImguiBackgroundStyle(BackgroundStyle backgroundStyle);
+	bool HasWindowFailedToCreate(GLFWwindow*& window);
 public:
 	void Draw();
 };
