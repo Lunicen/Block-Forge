f39c7d88129df331e65756700b56f0da7df0b022
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-07-26 00:45:53 +0200

    Add building Debug configuration

--- .github/setup_for_workflow.bat
+++ .github/setup_for_workflow.bat
@@ -9,18 +9,22 @@ echo Installing GLFW!
 
 :: x64 build
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A x64 -B %GLFW_build_dir%\x64
+cmake --build %GLFW_build_dir%\x64 --config Debug --clean-first
 cmake --build %GLFW_build_dir%\x64 --config Release --clean-first
 
 :: Win32 build
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A Win32 -B %GLFW_build_dir%\Win32
+cmake --build %GLFW_build_dir%\Win32 --config Debug --clean-first
 cmake --build %GLFW_build_dir%\Win32 --config Release --clean-first
 
 echo Installing GLAD!
 
 :: x64 build
 cmake %GLAD_root_dir% -B %GLAD_build_dir%\x64 -A x64
+cmake --build %GLAD_build_dir%\x64 --config Debug --clean-first
 cmake --build %GLAD_build_dir%\x64 --config Release --clean-first"
 
 :: Win32 build
 cmake %GLAD_root_dir% -B %GLAD_build_dir%\Win32 -A Win32
+cmake --build %GLAD_build_dir%\Win32 --config Debug --clean-first
 cmake --build %GLAD_build_dir%\Win32 --config Release --clean-first
