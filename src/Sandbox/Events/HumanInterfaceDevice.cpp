21f981ef790e4681e223427c0c2791d5f4d94c1f
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-08-02 10:46:38 +0200

    Add key codes description feature

--- src/Sandbox/Events/HumanInterfaceDevice.cpp
+++ src/Sandbox/Events/HumanInterfaceDevice.cpp
@@ -1,12 +1,50 @@
 #include "HumanInterfaceDevice.h"
 
+std::string HumanInterfaceDevice::KeyDescription(const KeyboardKey& key) const
+{
+	switch(key)
+	{
+	case KeyboardKey::a: return "A";
+	case KeyboardKey::d: return "D";
+	case KeyboardKey::s: return "S";
+	case KeyboardKey::w: return "W";
+	
+	case KeyboardKey::escape: return "ESC";
+	case KeyboardKey::space: return "SPACE";
+	case KeyboardKey::leftShift: return "LEFT SHIFT";
+	case KeyboardKey::rightShift: return "RIGHT SHIFT";
+	case KeyboardKey::leftCtrl: return "LEFT CTRL";
+	case KeyboardKey::rightCtrl: return "RIGHT CTRL";
+	}
+
+	_log.Warn("Key code " + std::to_string(static_cast<int>(key)) + " description not found!");
+	return std::to_string(static_cast<int>(key));
+}
+
+std::string HumanInterfaceDevice::ButtonDescription(const MouseButton& button) const
+{
+	switch(button)
+	{
+	case MouseButton::left: return "LEFT BUTTON";
+	case MouseButton::right: return "RIGHT BUTTON";
+	}
+
+	_log.Warn("Button code " + std::to_string(static_cast<int>(button)) + " description not found!");
+	return std::to_string(static_cast<int>(button));
+}
+
 HumanInterfaceDevice::HumanInterfaceDevice(GLFWwindow*& window) : _window(window)
 {
 }
 
 bool HumanInterfaceDevice::IsKeyPressed(const KeyboardKey& key) const
 {
-	return glfwGetKey(_window, static_cast<int>(key)) == GLFW_PRESS;
+	if (glfwGetKey(_window, static_cast<int>(key)) == GLFW_PRESS)
+	{
+		_log.Trace("Key pressed: " + KeyDescription(key));
+		return true;
+	}
+	return false;
 }
 
 bool HumanInterfaceDevice::IsKeyReleased(const KeyboardKey& key) const
@@ -16,7 +54,12 @@ bool HumanInterfaceDevice::IsKeyReleased(const KeyboardKey& key) const
 
 bool HumanInterfaceDevice::IsButtonPressed(const MouseButton& button) const
 {
-	return glfwGetMouseButton(_window, static_cast<int>(button)) == GLFW_PRESS;
+	if (glfwGetMouseButton(_window, static_cast<int>(button)) == GLFW_PRESS)
+	{
+		_log.Trace("Button pressed: " + ButtonDescription(button));
+		return true;
+	}
+	return false;
 }
 
 bool HumanInterfaceDevice::IsButtonReleased(const MouseButton& button) const
--- src/Sandbox/Events/HumanInterfaceDevice.h
+++ src/Sandbox/Events/HumanInterfaceDevice.h
@@ -1,10 +1,17 @@
 #pragma once
+#include "Core/Log.h"
 #include <GLFW/glfw3.h>
 #include "KeyCodes.h"
 
+
 class HumanInterfaceDevice
 {
+	Log& _log = Log::Get();
+
 	GLFWwindow*& _window;
+
+	std::string KeyDescription(const KeyboardKey& key) const;
+	std::string ButtonDescription(const MouseButton& button) const;
 
 public:
 	explicit HumanInterfaceDevice(GLFWwindow*& window);
