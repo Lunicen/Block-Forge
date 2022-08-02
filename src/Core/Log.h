aa404c94ad0c85e4cb4e6fa1f00dc4e50f867ac2
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-08-02 10:46:19 +0200

    Optimize Log class

--- src/Core/Log.h
+++ src/Core/Log.h
@@ -32,32 +32,32 @@ public:
 	///	@details The trace is used to print EVERYTHING that is happening.
 	///	@warning Never, ever use it in production! Only during the development process.
 	/// @param message - Information about the event.
-	void Trace(const std::string& message);
+	void Trace(const std::string& message) const;
 
 	/// @brief Prints debug message.
 	///	@details The debug is used to print the information for diagnostics.
 	/// @param message - Information about the event.
-	void Debug(const std::string& message);
+	void Debug(const std::string& message) const;
 
 	/// @brief Prints info message.
 	///	@details The info is used to print the general information about occurred events.
 	/// @param message - Information about the event.
-	void Info(const std::string& message);
+	void Info(const std::string& message) const;
 
 	/// @brief Prints warning message.
 	///	@details The warning is used to warn that the occurred event might cause oddities in the future.
 	/// @param message - Information about the event.
-	void Warn(const std::string& message);
+	void Warn(const std::string& message) const;
 
 	/// @brief Prints error message.
 	///	@details The error is used to signalize incorrect behaviour that can be handled (for ex. missing data). 
 	/// @param message - Information about the event.
-	void Error(const std::string& message);
+	void Error(const std::string& message) const;
 
 	/// @brief Prints critical message.
 	///	@details The critical is used to print the error that caused a force shutdown of the application.
 	/// @param message - Information about the event.
-	void Critical(const std::string& message);
+	void Critical(const std::string& message) const;
 
 	~Log() = default;
 };
