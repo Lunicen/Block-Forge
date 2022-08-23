6b1601fa388f105f6df76b53555ca25e36dd2f41
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-08-24 00:17:45 +0200

    Update workflows

--- .github/GLFW_workflow_setup.bat
+++ .github/GLFW_workflow_setup.bat
@@ -2,8 +2,6 @@
 
 set GLFW_root_dir=..\lib\glfw
 set GLFW_build_dir=..\lib\glfw\build
-set GLAD_root_dir=..\lib\glad
-set GLAD_build_dir=..\lib\glad\build
 
 echo Installing GLFW!
 
@@ -16,15 +14,3 @@ cmake --build %GLFW_build_dir%\x64 --config Release --clean-first
 cmake -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF %GLFW_root_dir% -A Win32 -B %GLFW_build_dir%\Win32
 cmake --build %GLFW_build_dir%\Win32 --config Debug --clean-first
 cmake --build %GLFW_build_dir%\Win32 --config Release --clean-first
-
-echo Installing GLAD!
-
-:: x64 build
-cmake %GLAD_root_dir% -B %GLAD_build_dir%\x64 -A x64
-cmake --build %GLAD_build_dir%\x64 --config Debug --clean-first
-cmake --build %GLAD_build_dir%\x64 --config Release --clean-first"
-
-:: Win32 build
-cmake %GLAD_root_dir% -B %GLAD_build_dir%\Win32 -A Win32
-cmake --build %GLAD_build_dir%\Win32 --config Debug --clean-first
-cmake --build %GLAD_build_dir%\Win32 --config Release --clean-first
