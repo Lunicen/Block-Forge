89ad8871ad0604f64a6afb7487e2b216117a6dbc
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-09-01 20:37:55 +0200

    Fix bug with wrong coordinates biome map generation

--- src/Sandbox/Utils/ProceduralGen/Noise2D.cpp
+++ src/Sandbox/Utils/ProceduralGen/Noise2D.cpp
@@ -36,7 +36,7 @@ std::vector<std::vector<float>> Noise2D::GetNoise(
 	const auto areaSize = frame.size + static_cast<size_t>(2) * expansionFactor;
 
 	const auto x = frame.origin.x * chunkSize + expansionFactor + xOffset;
-	const auto y = frame.origin.y * chunkSize + expansionFactor + yOffset;
+	const auto y = frame.origin.z * chunkSize + expansionFactor + yOffset;
 
 	auto noise = std::vector<float>(areaSize * areaSize);
 
