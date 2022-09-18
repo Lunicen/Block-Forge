433ecb2a871ceb85ac18a5f7a8fc5f57ca456bd8
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-09-18 13:23:22 +0200

    Setup mesh for instancing

--- src/Sandbox/Model/Mesh/Mesh.cpp
+++ src/Sandbox/Model/Mesh/Mesh.cpp
@@ -1,18 +1,25 @@
 #include "Mesh.h"
 
 Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<TriangleIndexes>& indices, Shader& shader)
-	: _vbo(VertexBuffer(vertices)), _ebo(ElementBuffer(indices)), _shader(shader)
+	: _shader(shader)
 {
+	const auto vbo = VertexBuffer(vertices);
+	const auto ebo = ElementBuffer(indices);
+
 	constexpr auto stride = sizeof(Vertex) / sizeof(float);
 	constexpr auto vector2dSize = 2;
 	constexpr auto vector3dSize = 3;
 
-	_vao.Link(_vbo, 0, vector3dSize, stride, 0);
-	_vao.Link(_vbo, 1, vector2dSize, stride, vector3dSize);
+	_vao.Link(vbo, 0, vector3dSize, stride, 0);
+	_vao.Link(vbo, 1, vector2dSize, stride, vector3dSize);
 
 	_vao.Unbind();
-	_vbo.Unbind();
-	_ebo.Unbind();
+	vbo.Unbind();
+	ebo.Unbind();
+}
+
+void Mesh::SetPositions(const std::vector<Position>& transformations)
+{
 }
 
 void Mesh::Bind() const
--- src/Sandbox/Model/Mesh/Mesh.h
+++ src/Sandbox/Model/Mesh/Mesh.h
@@ -8,12 +8,11 @@
 class Mesh
 {
 	VertexArray _vao;
-	VertexBuffer _vbo;
-	ElementBuffer _ebo;
 	Shader& _shader;
 
 public:
 	Mesh(const std::vector<Vertex>& vertices, const std::vector<TriangleIndexes>& indices, Shader& shader);
+	void SetPositions(const std::vector<Position>& transformations);
 
 	void Bind() const;
 	void Unbind() const;
