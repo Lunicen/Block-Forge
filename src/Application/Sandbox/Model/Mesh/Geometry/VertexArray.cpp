50b7b39ad458ae6eceb6c2f1f9ef98c51b6f4f68
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-09-30 15:02:09 +0200

    Refactor VAO class

--- src/Application/Sandbox/Model/Mesh/Geometry/VertexArray.cpp
+++ src/Application/Sandbox/Model/Mesh/Geometry/VertexArray.cpp
@@ -11,14 +11,15 @@ VertexArray::VertexArray()
 void VertexArray::Link(const VertexBuffer& vbo, const GLuint layout, const GLint size, const GLsizei stride, const GLint offset) const
 {
 	vbo.Bind();
+
 	glVertexAttribPointer(
 		layout, size,
 		GL_FLOAT, GL_FALSE,
 		static_cast<GLsizei>(stride * sizeof(float)),
 		reinterpret_cast<const void*>(offset * sizeof(float))  // NOLINT(performance-no-int-to-ptr)
 	);
-
 	glEnableVertexAttribArray(layout);
+
 	vbo.Unbind();
 }
 
