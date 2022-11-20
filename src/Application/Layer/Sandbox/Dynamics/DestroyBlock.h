139b9c04d19afda58962c05d8b6c0fce3a22fe6c
lukasz340 <lukasz.zaleski04@gmail.com>
2022-11-20 22:58:26 +0100

    Add documentation for place and destroy blocks

--- src/Application/Layer/Sandbox/Dynamics/DestroyBlock.h
+++ src/Application/Layer/Sandbox/Dynamics/DestroyBlock.h
@@ -4,10 +4,15 @@
 #include "../World/Chunks/ChunkUtils.h"
 #include "Application/Layer/Sandbox/World/Chunks/Structure/Chunk.h"
 
-
+/// @class DestroyBlock
+///	@brief The class that destroy blocks
 class DestroyBlock {
 
 	public:
+		/// @brief Destroy block in front of the camera
+		///	@param _orientation - orientation of the camera
+		///	@param _position - position of the camera
+		/// @param chunks - the map with chunks with their positions
+		///	@param blockMap - block map, which is used to place them inside chunks based on the biome noise.
 		static void Destroy(glm::vec3 _orientation, glm::vec3 _position, std::unordered_map<Position, std::unique_ptr<Chunk>>& chunks, BlockMap& blockMap);
-
 };
--- src/Application/Layer/Sandbox/Dynamics/PlaceBlock.h
+++ src/Application/Layer/Sandbox/Dynamics/PlaceBlock.h
@@ -1,9 +1,15 @@
 #pragma once
 #include "Application/Layer/Sandbox/World/Chunks/Structure/Chunk.h"
 
-
+/// @class PlaceBlock
+///	@brief The class that place new blocks
 class PlaceBlock {
 
 public:
+		/// @brief Destroy block in front of the camera
+		///	@param _orientation - orientation of the camera
+		///	@param _position - position of the camera
+		/// @param chunks - the map with chunks with their positions
+		///	@param blockMap - block map, which is used to place them inside chunks based on the biome noise.
 	static void Place(glm::vec3 _orientation, glm::vec3 _position, std::unordered_map<Position, std::unique_ptr<Chunk>>& chunks, BlockMap& blockmap);
 };
