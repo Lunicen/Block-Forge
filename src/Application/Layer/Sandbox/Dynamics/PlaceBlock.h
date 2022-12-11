d81a49ee25eb5aac834e395e08912810025ea3d9
Łukasz Zaleski <70091951+lukasz340@users.noreply.github.com>
2022-12-11 22:01:30 +0100

    Place different blocks (#77)
    * Place hardcoded block
    
    * Place and destroy hardcoded block when mouse pressed
    
    * Place and Destroy blocks in proggress
    
    * Destroying works
    
    * Place blocks at every chunk
    
    * IsPressedOnce instead of IsPressed, when placing/destroying block
    
    * little reafactor
    
    * Improve readability
    
    * Working place and destroy block
    
    * Destroying blocks works
    
    * Remove bug in destroying blocks
    
    * Destroy blocks working
    
    * Add documentation for place and destroy blocks
    
    * Fix typo
    
    * Add documentation
    
    * Add documentation
    
    * Fix error
    
    * Remove code smells
    
    * Remove code smell
    
    * Remove code smells
    
    * fixed bug
    
    * Fix bug
    
    * Integrate placing blocks with HUD
    
    * Add documentation
    
    * Remove code smells
    
    * Add documentation
    
    * Remove code smell
    
    * Remove code smell
    
    * Remove code smell
    
    * Little change

--- src/Application/Layer/Sandbox/Dynamics/PlaceBlock.h
+++ src/Application/Layer/Sandbox/Dynamics/PlaceBlock.h
@@ -1,15 +1,28 @@
 #pragma once
 #include "Application/Layer/Sandbox/World/Chunks/Structure/Chunk.h"
+#include "Application/Layer/Sandbox/Hud.h"
 
 /// @class PlaceBlock
 ///	@brief The class that place new blocks
 class PlaceBlock {
 
 public:
-	/// @brief Destroy block in front of the camera
+	/// @brief Place block in front of the camera
 	///	@param orientation - orientation of the camera
 	///	@param position - position of the camera
 	/// @param chunks - the map with chunks with their positions
 	///	@param blockMap - block map, which is used to place them inside chunks based on the biome noise.
-	static void PlaceBlock::Place(glm::vec3 orientation, glm::vec3 position, HashMap<Position, std::unique_ptr<Chunk>>& chunks, BlockMap& blockMap);
+	///	@param heldItem - type of block, which is used to place
+	static void PlaceBlock::Place(glm::vec3 orientation, glm::vec3 position, HashMap<Position, std::unique_ptr<Chunk>>& chunks, BlockMap& blockMap, const std::string& heldItem);
+
+	/// @brief Place block or continue iterating
+	///	@param chunkPosition - position of the chunk
+	///	@param chunkSize - size of the chunk
+	///	@param pos - position of the block
+	///	@param chunkPositionForNewBlock - position of the chunk for new block
+	/// @param chunks - the map with chunks with their positions
+	///	@param blockMap - block map, which is used to place them inside chunks based on the biome noise.
+	///	@param heldItem - type of block, which is used to place
+	static bool PlaceBlock:: PlaceBlockOnChunk(HashMap<Position, std::unique_ptr<Chunk>>& chunks, Position chunkPosition, glm::vec3& pos, size_t chunkSize, Position chunkPositionForNewBlock, BlockMap& blockMap, const std::string& heldItem);
+
 };
